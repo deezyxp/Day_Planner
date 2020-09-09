b4a3ca415d7cfb12a9d17ce8915553385b3acdfc
deezy <danielshiaolin@gmail.com>
2020-09-09 16:09:16 +1000

    add buttons to all input areas

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -4,7 +4,7 @@ function renderCalendar() {
       let momentVar = moment().format('dddd, MMMM Do, YYYY h:mm a');
       let workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM",
         "5PM"]
-  
+
         // countdown timer 
       setInterval(() => {
         let realTime = moment().format('dddd, MMMM Do, YYYY h:mm:ss a');
@@ -16,23 +16,21 @@ function renderCalendar() {
         let currentDay = $("#currentDay").text(momentVar);
         return currentDay;
       }
-  
+
     //   Display timeblocks for working hours 9-5
       for (var i = 0; i < workHours.length; i++) {
         let row = $("<div>").addClass("row");
         let timeCol = $("<text-area>").text(workHours[i]).addClass("hour");
         let inputCol = $("<input>").attr("placeholder", "Enter note here").addClass("time-block");
-
         const currentTimeblockMoment = moment(workHours[i], 'hA');
 
-        if(  currentTimeblockMoment < moment() && currentTimeblockMoment.clone().add(1, 'hA') < moment() ){
+        if(  currentTimeblockMoment < moment() && currentTimeblockMoment.clone().add(1, 'hours') < moment() ){
           // past
           inputCol = inputCol.addClass('past')
         }
-
-        if ( currentTimeblockMoment < moment() <  currentTimeblockMoment.clone().add(1, 'hA') ) {
+        
+        if ( currentTimeblockMoment < moment() && moment() <  currentTimeblockMoment.clone().add(1, 'hours') ) {
           // present
-          // we should put in current colour
           inputCol = inputCol.addClass('present')
         }
 
@@ -41,16 +39,128 @@ function renderCalendar() {
           inputCol = inputCol.addClass('future')
         }
 
-
-
         saveBtn = $("<button>").addClass("btn btn-primary saveBtn").text("SAVE");
         $(row).append(timeCol).append(inputCol).append(saveBtn);
         $("#calendar").append(row);
       }
 
-      
+  //for loop to add id's to input areas
+  function addIdToInput() {
+    let inputId = document.getElementsByClassName("toDo-input");
+    let length = inputId.length;
+    for (i = 0; i < length; i++) {
+      inputId[i].id = "input-area-" + (i + 1);
+    }
+  };
+  addIdToInput();
+
+  //for loop to add id's to save buttons
+  function addIdToSaveBtn() {
+    let saveBtnId = document.getElementsByClassName("saveBtn");
+    let length = saveBtnId.length;
+    for (i = 0; i < length; i++) {
+      saveBtnId[i].id = "saveBtnId-" + (i + 1);
+    }
+  }
+  addIdToSaveBtn();
+
+  let saveBtnId1 = $("#saveBtnId-1");
+  let saveBtnId2 = $("#saveBtnId-2");
+  let saveBtnId3 = $("#saveBtnId-3");
+  let saveBtnId4 = $("#saveBtnId-4");
+  let saveBtnId5 = $("#saveBtnId-5");
+  let saveBtnId6 = $("#saveBtnId-6");
+  let saveBtnId7 = $("#saveBtnId-7");
+  let saveBtnId8 = $("#saveBtnId-8");
+  let saveBtnId9 = $("#saveBtnId-9");
+
+  let toDoInput1 = $("#input-area-1");
+  let toDoInput2 = $("#input-area-2");
+  let toDoInput3 = $("#input-area-3");
+  let toDoInput4 = $("#input-area-4");
+  let toDoInput5 = $("#input-area-5");
+  let toDoInput6 = $("#input-area-6");
+  let toDoInput7 = $("#input-area-7");
+  let toDoInput8 = $("#input-area-8");
+  let toDoInput9 = $("#input-area-9");
 
 
+  let storedInput;
+
+  //click events for each save button 
+
+  $(saveBtnId1).on("click", function (event) {
+    event.preventDefault();
+    let toDoInput1 = $("#input-field-1").val().trim();
+    localStorage.setItem("toDo1", (toDoInput1));
+  });
+  let savedInput1 = (localStorage.getItem("toDo1"));
+  toDoInput1.val(savedInput1);
+
+  $(saveBtnId2).on("click", function (event) {
+    event.preventDefault();
+    let toDoInput2 = $("#input-field-2").val().trim();
+    localStorage.setItem("toDo2", (toDoInput2));
+  });
+  let savedInput2 = (localStorage.getItem("toDo2"));
+  toDoInput2.val(savedInput2);
+
+  $(saveBtnId3).on("click", function (event) {
+    event.preventDefault();
+    let toDoInput3 = $("#input-field-3").val().trim();
+    localStorage.setItem("toDo3", (toDoInput3));
+  });
+  let savedInput3 = (localStorage.getItem("toDo3"));
+  toDoInput3.val(savedInput3);
+
+  $(saveBtnId4).on("click", function (event) {
+    event.preventDefault();
+    let toDoInput4 = $("#input-field-4").val().trim();
+    localStorage.setItem("toDo4", (toDoInput4));
+  });
+  let savedInput4 = (localStorage.getItem("toDo4"));
+  toDoInput4.val(savedInput4);
+
+  $(saveBtnId5).on("click", function (event) {
+    event.preventDefault();
+    let toDoInput5 = $("#input-field-5").val().trim();
+    localStorage.setItem("toDo5", (toDoInput5));
+  });
+  let savedInput5 = (localStorage.getItem("toDo5"));
+  toDoInput5.val(savedInput5);
+
+  $(saveBtnId6).on("click", function (event) {
+    event.preventDefault();
+    let toDoInput6 = $("#input-field-6").val().trim();
+    localStorage.setItem("toDo6", (toDoInput6));
+  });
+  let savedInput6 = (localStorage.getItem("toDo6"));
+  toDoInput6.val(savedInput6);
+
+  $(saveBtnId7).on("click", function (event) {
+    event.preventDefault();
+    let toDoInput7 = $("#input-field-7").val().trim();
+    localStorage.setItem("toDo7", (toDoInput7));
+  });
+  let savedInput7 = (localStorage.getItem("toDo7"));
+  toDoInput7.val(savedInput7);
+
+  $(saveBtnId8).on("click", function (event) {
+    event.preventDefault();
+    let toDoInput8 = $("#input-field-8").val().trim();
+    localStorage.setItem("toDo8", (toDoInput8));
+  });
+  let savedInput8 = (localStorage.getItem("toDo8"));
+  toDoInput8.val(savedInput8);
+
+  $(saveBtnId9).on("click", function (event) {
+    event.preventDefault();
+    let toDoInput9 = $("#input-field-8").val().trim();
+    localStorage.setItem("toDo9", (toDoInput9));
+  });
+  let savedInput9 = (localStorage.getItem("toDo9"));
+  toDoInput9.val(savedInput9);
 
   })}
+
   renderCalendar();
